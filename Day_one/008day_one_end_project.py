3a3aa56c2dfd760c1a70cdf7a385b46e4b23cfb5
Uduh-oc <oluchukwuuduh3@gmail.com>
2024-05-28 18:25:17 +0100

    Add files via upload

---
+++ Day_one/008day_one_end_project.py
@@ -0,0 +1,4 @@
+print("whats good bro, well be making a band name for you today")
+favourite_anime = input("what anime did you grow up wih? \n")
+favourite_gun = input("what is your favoutite gun? \n")
+print("your band name should be \n", favourite_anime, favourite_gun, "hope you like it" )
