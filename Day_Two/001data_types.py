6721ba6eb0dff2686c7da83274bb6ac8d48cb43d
Uduh-oc <oluchukwuuduh3@gmail.com>
2024-05-28 22:35:34 +0100

    learning about data types in this lesson

---
+++ Day_Two/001data_types.py
@@ -0,0 +1,19 @@
+#Daya types
+
+#String
+print("Hello niggas"[6])
+#this is an example of subscripting, the number in the '[]' determines the character brought out from the string, count from zero
+"123" #string
+
+#Integer whole numbers
+print(123 + 345)
+print(123_456_678)
+
+#float
+
+3.14159
+
+#boolean
+
+True
+False
