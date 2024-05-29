ab77cc773d5bf98084e669729233422bbe3202ae
Uduh-oc <oluchukwuuduh3@gmail.com>
2024-05-29 11:12:11 +0100

    type checking and type conversion

---
+++ Day_Two/002type_error_typechecking_typeconversion.py
@@ -0,0 +1,13 @@
+# len(27484) 
+num_char = len(input("what is your name? "))
+#type(...) this checks the type of data
+
+#type conversion
+new_num_char = str(num_char)
+
+##str(...) changes the data type into a string
+print("Your name has " + new_num_char + " characters.")
+
+##float(...) changes what ever is inside the parenthesis into a float
+
+print(60 + float("2020.5575"))
