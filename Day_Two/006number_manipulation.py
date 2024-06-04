30a577febd38720f757e8bb56972fed2a7833f6a
Uduh-oc <oluchukwuuduh3@gmail.com>
2024-06-04 21:10:26 +0100

    Add files via upload

---
+++ Day_Two/006number_manipulation.py
@@ -0,0 +1,8 @@
+# print((round(8 / 3, 4)))
+# "//" chnges it srtaight to integer instead of float
+result=56 / 7
+#sign in front of equals before a variable i.e "a +=" carries the mathematical function on the variable
+result += 2
+print(f"your score is {result}, ", result)
+#f-String converts a data type to string in a more convinient manner
+
